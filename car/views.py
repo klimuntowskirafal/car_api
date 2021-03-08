e7288563f29dbdc016c282ad978ca2ffe0c2f8a4
klimuntowskirafal <51902773+klimuntowskirafal@users.noreply.github.com>
2021-03-08 09:34:14 +0100

    Updates (#7)
    * Car model update
    
    * view RateACar update
    
    * added templates

--- car/views.py
+++ car/views.py
@@ -1,11 +1,18 @@
 from django.db.models import Count
 from django.http import JsonResponse
+from django.shortcuts import render
+
 from .models import Car, Rate
 from rest_framework import status
 from rest_framework.views import APIView, Response
 
 from .methods import get_all_cars_with_calculated_avg_rating, \
     add_new_car_if_found_in_external_api, get_car_object, logger
+
+
+def car_api_main_page(request):
+    if request.method == "GET":
+        return render(request, "car_api_introduction.html")
 
 
 class CarApi(APIView):
@@ -40,8 +47,7 @@ class RateACar(APIView):
             rating = request.data["rating"]
 
             # validate if passed request rating is integer, return ValueError
-            # if not
-            if isinstance(rating, float):
+            if not isinstance(rating, int):
                 logger.warning(
                     f"rating value must be an integer: {request.data}")
                 return Response(status=status.HTTP_400_BAD_REQUEST)
